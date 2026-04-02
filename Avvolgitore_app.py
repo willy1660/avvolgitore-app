ddf1f9177eb127ec21e39a1663b207e94d1c359d
willy1660 <willysegurap@gmail.com>
2026-04-02 11:11:49 +0200

    Update Avvolgitore_app.py

--- Avvolgitore_app.py
+++ Avvolgitore_app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Avvolgimento", layout="wide")
 # HEADER
 # =========================
 
-col_logo, col_title = st.columns([1,2])
+col_logo, col_title = st.columns([1,5])
 
 logo_path = os.path.join(os.path.dirname(__file__), "New Logo PDM - rame.png")
 
