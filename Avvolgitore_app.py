efce4939e2ba7b2512e43408f199a88fbc507e1d
willy1660 <willysegurap@gmail.com>
2026-04-09 15:47:38 +0200

    Update Avvolgitore_app.py

--- Avvolgitore_app.py
+++ Avvolgitore_app.py
@@ -464,7 +464,7 @@ def viewer(
         controls.rotateSpeed = 3.8;
         controls.zoomSpeed = 0.8;
         controls.panSpeed = 0.1;
-        controls.dynamicDampingFactor = 0.2;
+        controls.dynamicDampingFactor = 0.5;
         controls.staticMoving = false;
         controls.noPan = false;
         controls.noZoom = false;
