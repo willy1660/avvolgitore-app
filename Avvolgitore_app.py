2539b5610f95f30e8174d8acf8a9c69ac7120510
willy1660 <willysegurap@gmail.com>
2026-04-09 15:33:56 +0200

    Update Avvolgitore_app.py

--- Avvolgitore_app.py
+++ Avvolgitore_app.py
@@ -443,7 +443,7 @@ def viewer(
         const activeTubeColor = gelwhite ? 0xf0f0ee : 0x050505;
 
         const camera = new THREE.PerspectiveCamera(32, W / Hview, 0.1, 20000);
-        camera.position.set(-1000, -2000, 100);
+        camera.position.set(-1300, -2000, 150);
         camera.up.set(0, 0, 1);
 
         const renderer = new THREE.WebGLRenderer({{
