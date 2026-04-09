2cfb59b05d4f2ba6d0ce800412adb9e02bbf65e6
willy1660 <willysegurap@gmail.com>
2026-04-09 15:16:21 +0200

    Update Avvolgitore_app.py

--- Avvolgitore_app.py
+++ Avvolgitore_app.py
@@ -443,7 +443,7 @@ def viewer(
         const activeTubeColor = gelwhite ? 0xf0f0ee : 0x050505;
 
         const camera = new THREE.PerspectiveCamera(32, W / Hview, 0.1, 20000);
-        camera.position.set(600, -600, 900);
+        camera.position.set(-820, -520, 260);
 
         const renderer = new THREE.WebGLRenderer({{
             antialias: true,
@@ -468,7 +468,6 @@ def viewer(
         controls.noPan = false;
         controls.noZoom = false;
         controls.noRotate = false;
-        controls.target.set(0, 0, {spalla}/2);
 
         const R = {float(d_aspo)} / 2.0;
         const Rt = {float(d_tubo)} / 2.0;
@@ -479,6 +478,8 @@ def viewer(
         const aspoMode = {aspo_mode_json};
         const showGrid = {show_grid_json};
         const showAxes = {show_axes_json};
+
+        controls.target.set(40, 40, Hs * 0.52);
 
         const localRaw = {final_local_points_json};
         const thetaRaw = {final_thetas_json};
